f8451ee451aee64f8ad5e292357af06778092c63
Gleb Gaydardzhiev <glebster783@yandex.ru>
2020-11-28 01:21:08 +0300

    Initial commit

---
+++ deposit-service/src/main/java/com/javastart/deposit/DepositService.java
@@ -0,0 +1,7 @@
+package com.javastart.deposit;
+
+public class DepositService {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ registry-service/src/main/java/com/javastart/RegistryApplication.java
@@ -0,0 +1,7 @@
+package com.javastart;
+
+public class RegistryApplication {
+    public static void main(String[] args) {
+
+    }
+}
